34a635150986aa63f605039056f4888002e8d020
Selam Alem <selamtalem@gmail.com>
2021-10-21 01:16:24 -0500

    After 300 hours, i have FINALLY mastered how to log each new addition

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,28 +1,37 @@
 console.log('***** Music Collection *****')
 
 //created a new variable called collection with an empty array.
-let collection = 0;
+let collection = [];
 //end collection as empty
 
 //create a funtion names addToCollection
-function addToCollection (){
+function addToCollection ( title, artist, yearPublished) {
   let album = {
-    title: 'Yasteseryal',
-    artist: 'Teddy Afro',
-    yearPublished: 2005,
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
 };
-   collection = album;
-   return album++
-}//end function addToCollection
+collection.push(album);
+return album;
+}
 
-addToCollection(collection);
-console.log( 'Collection array now has:', collection);
+//Here i will be adding my 6 albums:
+addToCollection('Take Time', 'Giveon', 2020 );
+console.log('Added:', collection[0]);
 
+addToCollection('Over It', 'Summer Walker', 2019 );
+console.log('Added:', collection[1]);
 
-//adding 6 more albums to my collection using the addToCollection function
-//addToCollection(collection)
-// albumInfo.title = 'Take Time';
-// albumInfo.artist = 'Giveon';
-// albumInfo.yearPublished= 2020,
-// console.log( 'Added 2nd album to collection:', [albumInfo.title2, albumInfo.artist2, albumInfo.yearPublished2]);
-// , 'Over It', 'Triology', 'Take Care', 'Aster', 'Seven Days' ]
+addToCollection('Trilogy', 'The Weeknd', 2012);
+console.log('Added:', collection[2]);
+
+addToCollection('Take Care', 'Drake', 2011);
+console.log('Added:', collection[3]);
+
+addToCollection('Aster', 'Aster', 1989);
+console.log('Added:', collection[4]);
+
+addToCollection('Seven Days', 'PartyNextDoor', 2017);
+console.log('Added:', collection[5]);
+
+console.log('The Full Collection:', collection);
