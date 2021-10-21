b0cbd15f9659e5bce3cb557682e85070fd8d791f
Selam Alem <selamtalem@gmail.com>
2021-10-20 21:03:05 -0500

    I changed the first required feauture

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,16 +1,28 @@
 console.log('***** Music Collection *****')
 
+//created a new variable called collection with an empty array.
 let collection = 0;
+//end collection as empty
 
+//create a funtion names addToCollection
 function addToCollection (){
-   collection = albumInfo;
-}
-
-let albumInfo = {
-  title: 'Yasteseryal',
-  artist: 'Teddy Afro',
-  yearPublished: 2005,
+  let album = {
+    title: 'Yasteseryal',
+    artist: 'Teddy Afro',
+    yearPublished: 2005,
 };
+   collection = album;
+   return album++
+}//end function addToCollection
 
-addToCollection(albumInfo)
+addToCollection(collection);
 console.log( 'Collection array now has:', collection);
+
+
+//adding 6 more albums to my collection using the addToCollection function
+//addToCollection(collection)
+// albumInfo.title = 'Take Time';
+// albumInfo.artist = 'Giveon';
+// albumInfo.yearPublished= 2020,
+// console.log( 'Added 2nd album to collection:', [albumInfo.title2, albumInfo.artist2, albumInfo.yearPublished2]);
+// , 'Over It', 'Triology', 'Take Care', 'Aster', 'Seven Days' ]
