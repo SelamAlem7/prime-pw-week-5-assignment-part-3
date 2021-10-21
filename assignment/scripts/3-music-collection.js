8a2f47841f6f550f9d3a3e54cb193e345ec0744d
Selam Alem <selamtalem@gmail.com>
2021-10-21 04:16:59 -0500

    I have no idea what is happening at this point

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -34,18 +34,17 @@ console.log('Added:', collection[4]);
 addToCollection('Seven Days', 'PartyNextDoor', 2017);
 console.log('Added:', collection[5]);
 
-console.log('The Full Collection:', collection[0,5]);
+console.log('The Full Collection:', collection);
 
 //Creating showCollection functions
 function showCollection (array) {
   console.log('Number of items in the array', array.length);
   let i = 0
   while (i < 7) {
-    console.log(array.title, 'by', array.artist, 'published in', array.yearPublished,);
+    console.log(collection[0,1], 'by', collection[0,2], 'published in', collection[0,3],);
     i++;
   }
 };
-
 // for (let i = 0; i < array.length; i++){
 //    console.log((array.title), 'by', array, 'published in',  );
 //    return array[i];
@@ -53,3 +52,38 @@ function showCollection (array) {
 
 //[array.title, 'by', +array.artist, 'published in', +array.yearPublished,]
 showCollection(collection);
+
+
+// function find(value, array) {
+//    for(let i = 0; i < arrray.length; i++ ) {
+//       if(i === array[i]) {
+//          return true;
+//       }
+//    }
+//    return false;
+// }
+
+function findByArtist(artist){
+  let artistSearch = 0;
+  for (let i = 0; i < collection.length; i++ ){
+    const artist = collection[i];
+    if(artist === artistSearch)
+    console.log(i);
+  }
+}
+
+
+//   for (let i = 0; i < collection.length; i++ ) {
+//     if(i === collection[i]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// let artistSearch = collection;
+findByArtist(collection);
+console.log(findByArtist("The Weeknd", collection));
+
+// let favNumbers = ["2","23","7","17","9"];
+// console.log(find("7", favNumbers));
