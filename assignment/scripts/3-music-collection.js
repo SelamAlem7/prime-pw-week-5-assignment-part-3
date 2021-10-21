cc575ed0f122bad6586871b65d2da1a79ffd20d0
Selam Alem <selamtalem@gmail.com>
2021-10-20 19:53:17 -0500

    completed first Required Feature

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+
+let collection = 0;
+
+function addToCollection (){
+   collection = albumInfo;
+}
+
+let albumInfo = {
+  title: 'Yasteseryal',
+  artist: 'Teddy Afro',
+  yearPublished: 2005,
+};
+
+addToCollection(albumInfo)
+console.log( 'Collection array now has:', collection);
